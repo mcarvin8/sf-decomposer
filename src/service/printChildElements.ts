d8fde613401639433dbd7b188623eb22e2b744e4
Matt Carvin <90224411+mcarvin8@users.noreply.github.com>
2024-01-30 11:29:55 -0500

    refactor: define json parser options once

--- src/service/printChildElements.ts
+++ src/service/printChildElements.ts
@@ -2,17 +2,9 @@
 
 import { XMLBuilder } from 'fast-xml-parser';
 import { INDENT } from '../helpers/constants.js';
-import { XML_PARSER_OPTION } from '../types/xmlParserOptions.js';
+import { JSON_PARSER_OPTION } from '../types/jsonParserOptions.js';
 
 export function printChildElements(element: any, indentLevel: number = 2): string {
-  const JSON_PARSER_OPTION = {
-    ...XML_PARSER_OPTION,
-    format: true,
-    indentBy: INDENT,
-    suppressBooleanAttributes: false,
-    suppressEmptyNode: false,
-  };
-
   const xmlBuilder = new XMLBuilder(JSON_PARSER_OPTION);
   const xmlString = xmlBuilder.build(element);
 
