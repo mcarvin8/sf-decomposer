4af78136fc7576c43fe62d100196e702a1ed6770
Matt Carvin <90224411+mcarvin8@users.noreply.github.com>
2024-01-30 14:00:41 -0500

    chore: address linting concerns with field names function

--- src/service/findFieldName.ts
+++ src/service/findFieldName.ts
@@ -1,20 +1,31 @@
-/* eslint-disable */
+'use strict';
 
-export function findFieldName(element: any, fieldNames: string): string | undefined {
+interface ElementWithFieldNames {
+  [key: string]: string | ElementWithFieldNames;
+}
+
+export function findFieldName(element: ElementWithFieldNames, fieldNames: string): string | undefined {
   const fieldNamesArray = fieldNames.split(',');
 
   for (const fieldName of fieldNamesArray) {
     // Check if the current fieldName exists in the element
-    if (element[fieldName]) {
-      return element[fieldName];
+    if (element[fieldName] !== undefined) {
+      if (typeof element[fieldName] === 'string') {
+        return element[fieldName] as string;
+      } else if (typeof element[fieldName] === 'object') {
+        const childFieldName = findFieldName(element[fieldName] as ElementWithFieldNames, fieldNames);
+        if (childFieldName !== undefined) {
+          return childFieldName;
+        }
+      }
     }
   }
 
   // Iterate through child elements to find the field name
   for (const key in element) {
-    if (typeof element[key] === 'object') {
-      const childFieldName = findFieldName(element[key], fieldNames);
-      if (childFieldName) {
+    if (typeof element[key] === 'object' && element[key] !== null) {
+      const childFieldName = findFieldName(element[key] as ElementWithFieldNames, fieldNames);
+      if (childFieldName !== undefined) {
         return childFieldName;
       }
     }
