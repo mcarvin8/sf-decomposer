e1bd33d7e3a2e4c06bc3742faf0c1f56ac9c3f2d
Matt Carvin <90224411+mcarvin8@users.noreply.github.com>
2024-02-14 16:47:28 -0500

    fix: resolve lint issues on  buildNestedElements

--- src/service/buildNestedElements.ts
+++ src/service/buildNestedElements.ts
@@ -1,4 +1,4 @@
-/* eslint-disable */
+'use strict';
 
 import { XMLBuilder } from 'fast-xml-parser';
 import { INDENT } from '../helpers/constants.js';
@@ -6,10 +6,10 @@ import { XmlElement, JSON_PARSER_OPTION } from './types.js';
 
 export function buildNestedElements(element: XmlElement, indentLevel: number = 2): string {
   const xmlBuilder = new XMLBuilder(JSON_PARSER_OPTION);
-  const xmlString = xmlBuilder.build(element);
+  const xmlString = xmlBuilder.build(element) as string;
 
   // Manually format the XML string with the desired indentation
-  const formattedXml = xmlString
+  const formattedXml: string = xmlString
     .split('\n')
     .map((line: string) => `${' '.repeat(indentLevel * INDENT.length)}${line}`)
     .join('\n')
