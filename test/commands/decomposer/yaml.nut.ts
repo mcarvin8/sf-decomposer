a39afd5db81c25ef20b2b5df50819ce01af0567e
Matt Carvin <90224411+mcarvin8@users.noreply.github.com>
2024-10-21 14:17:15 -0400

    test: add NUT test for windows build only until ubuntu setup issues are resolved

---
+++ test/commands/decomposer/yaml.nut.ts
@@ -0,0 +1,62 @@
+'use strict';
+/* eslint-disable no-await-in-loop */
+
+import { rm, writeFile } from 'node:fs/promises';
+import { copy } from 'fs-extra';
+
+import { execCmd, TestSession } from '@salesforce/cli-plugins-testkit';
+import { expect } from 'chai';
+
+import { METADATA_UNDER_TEST, SFDX_CONFIG_FILE } from './constants.js';
+import { compareDirectories } from './compareDirectories.js';
+
+describe('decomposer NUTs YAML format', () => {
+  let session: TestSession;
+
+  const originalDirectory: string = 'test/baselines';
+  const mockDirectory: string = 'yaml';
+  const configFile = {
+    packageDirectories: [{ path: 'yaml', default: true }],
+    namespace: '',
+    sfdcLoginUrl: 'https://login.salesforce.com',
+    sourceApiVersion: '58.0',
+  };
+  const configJsonString = JSON.stringify(configFile, null, 2);
+
+  before(async () => {
+    session = await TestSession.create({ devhubAuthStrategy: 'NONE' });
+    await copy(originalDirectory, mockDirectory, { overwrite: true });
+    await writeFile(SFDX_CONFIG_FILE, configJsonString);
+  });
+
+  after(async () => {
+    await session?.clean();
+    await rm(mockDirectory, { recursive: true });
+    await rm(SFDX_CONFIG_FILE);
+  });
+
+  it('should decompose all metadata types under test in YAML format, then delete the original files', async () => {
+    for (const metadataType of METADATA_UNDER_TEST) {
+      const command = `decomposer decompose --metadata-type "${metadataType}" --postpurge --prepurge --debug --format "yaml"`;
+      const output = execCmd(command, { ensureExitCode: 0 }).shellOutput.stdout;
+
+      expect(output.replace('\n', '')).to.equal(
+        `All metadata files have been decomposed for the metadata type: ${metadataType}`
+      );
+    }
+  });
+  it('should recompose all metadata types under test from the YAML files', async () => {
+    for (const metadataType of METADATA_UNDER_TEST) {
+      const command = `decomposer recompose --metadata-type "${metadataType}" --postpurge --debug --format "yaml"`;
+      const output = execCmd(command, { ensureExitCode: 0 }).shellOutput.stdout;
+
+      expect(output.replace('\n', '')).to.equal(
+        `All metadata files have been recomposed for the metadata type: ${metadataType}`
+      );
+    }
+  });
+
+  it('should confirm the recomposed files in a mock directory match the reference files (force-app)', async () => {
+    await compareDirectories(originalDirectory, mockDirectory);
+  });
+});
