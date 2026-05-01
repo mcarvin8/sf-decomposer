af79aff14241423b8a2c063552f31509d07254a8
github-action-benchmark <github@users.noreply.github.com>
2026-05-01 15:09:26 +0000

    add Decompose Runtime (customSmallerIsBetter) benchmark result for b039fe575d54671f4db9c7eba695d08aa4f8d7e9

---
+++ dev/bench/runtime/data.js
@@ -0,0 +1,110 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1777648166184,
+  "repoUrl": "https://github.com/mcarvin8/sf-decomposer",
+  "entries": {
+    "Decompose Runtime": [
+      {
+        "commit": {
+          "author": {
+            "name": "Matt Carvin",
+            "username": "mcarvin8",
+            "email": "90224411+mcarvin8@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "Matt Carvin",
+            "username": "mcarvin8",
+            "email": "90224411+mcarvin8@users.noreply.github.com"
+          },
+          "id": "b039fe575d54671f4db9c7eba695d08aa4f8d7e9",
+          "message": "chore(test): temp allow dispatch workflow to upload perf data to create baseline",
+          "timestamp": "2026-05-01T15:04:37Z",
+          "url": "https://github.com/mcarvin8/sf-decomposer/commit/b039fe575d54671f4db9c7eba695d08aa4f8d7e9"
+        },
+        "date": 1777648165480,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "small.xml.decompose.pass1",
+            "value": 226.47,
+            "unit": "ms"
+          },
+          {
+            "name": "small.xml.recompose.pass1",
+            "value": 637.92,
+            "unit": "ms"
+          },
+          {
+            "name": "small.xml.decompose.pass2",
+            "value": 219.8,
+            "unit": "ms"
+          },
+          {
+            "name": "small.xml.recompose.pass2",
+            "value": 627.55,
+            "unit": "ms"
+          },
+          {
+            "name": "small.json.decompose.pass1",
+            "value": 187.3,
+            "unit": "ms"
+          },
+          {
+            "name": "small.json.recompose.pass1",
+            "value": 158.34,
+            "unit": "ms"
+          },
+          {
+            "name": "small.json.decompose.pass2",
+            "value": 192.51,
+            "unit": "ms"
+          },
+          {
+            "name": "small.json.recompose.pass2",
+            "value": 162.56,
+            "unit": "ms"
+          },
+          {
+            "name": "small.json5.decompose.pass1",
+            "value": 222.48,
+            "unit": "ms"
+          },
+          {
+            "name": "small.json5.recompose.pass1",
+            "value": 167.11,
+            "unit": "ms"
+          },
+          {
+            "name": "small.json5.decompose.pass2",
+            "value": 187.64,
+            "unit": "ms"
+          },
+          {
+            "name": "small.json5.recompose.pass2",
+            "value": 163.86,
+            "unit": "ms"
+          },
+          {
+            "name": "small.yaml.decompose.pass1",
+            "value": 222.41,
+            "unit": "ms"
+          },
+          {
+            "name": "small.yaml.recompose.pass1",
+            "value": 185.95,
+            "unit": "ms"
+          },
+          {
+            "name": "small.yaml.decompose.pass2",
+            "value": 203.65,
+            "unit": "ms"
+          },
+          {
+            "name": "small.yaml.recompose.pass2",
+            "value": 186.46,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
