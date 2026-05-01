ed281e9ee160407064f97aeb2f2b9164ea513cfa
github-action-benchmark <github@users.noreply.github.com>
2026-05-01 15:09:28 +0000

    add Decompose Memory (customSmallerIsBetter) benchmark result for b039fe575d54671f4db9c7eba695d08aa4f8d7e9

---
+++ dev/bench/memory/data.js
@@ -0,0 +1,110 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1777648168164,
+  "repoUrl": "https://github.com/mcarvin8/sf-decomposer",
+  "entries": {
+    "Decompose Memory": [
+      {
+        "commit": {
+          "author": {
+            "name": "Matt Carvin",
+            "username": "mcarvin8",
+            "email": "90224411+mcarvin8@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "Matt Carvin",
+            "username": "mcarvin8",
+            "email": "90224411+mcarvin8@users.noreply.github.com"
+          },
+          "id": "b039fe575d54671f4db9c7eba695d08aa4f8d7e9",
+          "message": "chore(test): temp allow dispatch workflow to upload perf data to create baseline",
+          "timestamp": "2026-05-01T15:04:37Z",
+          "url": "https://github.com/mcarvin8/sf-decomposer/commit/b039fe575d54671f4db9c7eba695d08aa4f8d7e9"
+        },
+        "date": 1777648168135,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "small.xml.decompose.pass1",
+            "value": 10.125,
+            "unit": "MB"
+          },
+          {
+            "name": "small.xml.recompose.pass1",
+            "value": 1.09,
+            "unit": "MB"
+          },
+          {
+            "name": "small.xml.decompose.pass2",
+            "value": 3.75,
+            "unit": "MB"
+          },
+          {
+            "name": "small.xml.recompose.pass2",
+            "value": 9.336,
+            "unit": "MB"
+          },
+          {
+            "name": "small.json.decompose.pass1",
+            "value": 0.5,
+            "unit": "MB"
+          },
+          {
+            "name": "small.json.recompose.pass1",
+            "value": 0.156,
+            "unit": "MB"
+          },
+          {
+            "name": "small.json.decompose.pass2",
+            "value": 1,
+            "unit": "MB"
+          },
+          {
+            "name": "small.json.recompose.pass2",
+            "value": 0,
+            "unit": "MB"
+          },
+          {
+            "name": "small.json5.decompose.pass1",
+            "value": 0,
+            "unit": "MB"
+          },
+          {
+            "name": "small.json5.recompose.pass1",
+            "value": 1.199,
+            "unit": "MB"
+          },
+          {
+            "name": "small.json5.decompose.pass2",
+            "value": 0,
+            "unit": "MB"
+          },
+          {
+            "name": "small.json5.recompose.pass2",
+            "value": 0,
+            "unit": "MB"
+          },
+          {
+            "name": "small.yaml.decompose.pass1",
+            "value": 0.125,
+            "unit": "MB"
+          },
+          {
+            "name": "small.yaml.recompose.pass1",
+            "value": 0.5,
+            "unit": "MB"
+          },
+          {
+            "name": "small.yaml.decompose.pass2",
+            "value": 0.125,
+            "unit": "MB"
+          },
+          {
+            "name": "small.yaml.recompose.pass2",
+            "value": 0.051,
+            "unit": "MB"
+          }
+        ]
+      }
+    ]
+  }
+}
